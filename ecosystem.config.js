a051edfc892cd9218e3728873b700dafda34a92c
Julio Cesar Andres Campos Romero <julioandrescampos@gmail.com>
2022-01-28 15:58:04 -0300

    ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+    apps: [{
+        name: `${port} - ${project}`,
+        script: `/var/node/${project}/source/app.js`,
+        kill_timeout: 3000,
+        instances: 1,
+        output: `/var/node_logs/${project}-out.log`,
+        error: `/var/node_logs/${project}-error.log`,
+        merge_logs: true,
+        log_date_format: "DD-MM-YYYY HH:mm:ss:SSS",
+        env: {
+            NODE_ENV: "production"
+        }
+    }],
+    deploy: {
+         production: {
+         "host": "hansolo.sodimac.cl",
+         "user": "desarrollo",
+         "ref": "origin/master",
+         "repo": repository,
+         "path": `/var/node/${project}`,
+         "post-deploy": "npm install; pm2 startOrRestart ecosystem.config.js"
+         },
+        develop: {
+            "host": "thanos.dda.sodimac.cl",
+            "user": "desarrollo",
+            "ref": "origin/develop",
+            "repo": repository,
+            "path": `/var/node/${project}`,
+            "post-deploy": "npm install; pm2 startOrRestart ecosystem.config.js",
+        }
+    }
+}
